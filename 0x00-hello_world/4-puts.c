0dd9d4c9e1aa84ec29478c3fba68d77d1061169a
akinyemi07 <soldinixpresini@gmail.com>
2022-10-20 09:52:50 -0700

    this is so fraustrating

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -  void entry point
+ * Return - the function returns a zero
+ */
+
+int main(void)
+{
+	puts("programming is like building a multilingual puzzle");
+
+		return (0);
+
+}
