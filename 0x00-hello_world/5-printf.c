a5c8514ab8e219557ae8044b29bb803d099bc6b2
akinyemi07 <soldinixpresini@gmail.com>
2022-10-21 07:51:38 -0700

    corrections

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - a program to print a standard output
+ *
+ * Return: Always returns 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+		return (0);
+}
