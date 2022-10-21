2423fdb9d33cf34cfd1622715219b4487a396719
akinyemi07 <soldinixpresini@gmail.com>
2022-10-21 07:34:54 -0700

    corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
  * main -  void entry point
- * Return - the function returns a zero
+ *
+ * Return: Always 0
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 		return (0);
 
---
+++ 0x00-hello_world/new.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+	/**
+	 * main - Entry point
+	 *
+	 * Return: Always 0 (Success)
+	 *                                                                               */
+	int main(void)
+	{
+
+		char programming[] = "\"Programming is like building a multilingul puzzle";
+
+		puts(programming);
+
+		return (0);
